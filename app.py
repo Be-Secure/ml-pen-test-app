098ccd97267248098443d4108c9e1ca931b44053
Arun Suresh <58501088+asa1997@users.noreply.github.com>
2023-12-04 08:05:11 +0530

    Updated code to automatically push reports to github once available (#2)
    * Removed duplicate code
    
    * Added code to push file to github
    
    * Added requirements.txt
    
    * Revert "Added code to push file to github"
    
    This reverts commit 54a7a55b734dcbfb4128c331155d8540fb11f82f.
    
    * Updated
    
    * Added gitignore
    
    * Updated to automatically push reports to github

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import os
+import threading
 from urllib.parse import urlparse
 import requests
 import aishield as ais
 from flask import Flask, request
 import json
 import base64
+import schedule
+import time
 # Create a Flask app instance
 app = Flask(__name__)
 pypi = True
+
+org_id = '#######' #### Paste org id
+x_api_key = '#########'
+
+job_meta_data_path = 'models/bes-image-classification/fuzz-test/JobMetadata.json'
 
 def init_aishield(org_id_val):
     """
@@ -54,15 +62,20 @@ def register_model(task:str, attack_type:str, aishield_client):
     
     return status, model_registration_repsone, task_type, analysis_type
 
-def create_dir(dir):
-    if os.path.isdir(dir):
-        print("directory {} already exist".format(dir))
-    if os.path.isdir(dir) is False:
-        os.mkdir(path=dir)
-        print("directory {} created successfully".format(dir))
+def create_dir(dir_name):
+    '''
+        Method creates directory on the passed parameter.
+    '''
+    if os.path.isdir(dir_name):
+        print("directory {} already exist".format(dir_name))
+    if os.path.isdir(dir_name) is False:
+        os.mkdir(path=dir_name)
+        print("directory {} created successfully".format(dir_name))
 
 def download_zip_files(*args):
-        
+    '''
+        Downloads the model.zip, label.zip and data.zip file from repository.
+    '''
     zip_path=os.environ['HOME'] +'/zip'
     create_dir(zip_path)
     for item in args:
@@ -100,13 +113,13 @@ def upload_artifacts(zip_path, status, model_registration_repsone, aishield_clie
         print("else")
 
 def model_analysis(task_type, analysis_type, aishield_client, model_registration_repsone):
+    """
+    Description: Specify the appropriate configs required for vulnerability analysis and trigger model analysis
+    """
     img_row,img_col,channel=28,28,1
     num_classes=10
     input_shape=(img_row,img_col,channel)
     model_encryption=0 #0 if model is uploaded directly as a zip, 1 if model is encryted as .pyc and uploaded as a zip
-    """
-    Description: Specify the appropriate configs required for vulnerability analysis and trigger model analysis
-    """
 
 
     if pypi:
@@ -137,42 +150,46 @@ def model_analysis(task_type, analysis_type, aishield_client, model_registration
     return job_id
 
 
-def download_analysis_reports(status, report_path, aishield_client, job_id):
+# def download_analysis_reports(status, report_path, aishield_client, job_id):
     
-    if status == "success":
-        output_conf_vuln = ais.OutputConf(report_type=ais.get_type("report", "vulnerability"),
-                                     file_format=ais.get_type("file_format", "json"),
-                                     save_folder_path=report_path)
+#     if status == "success":
+#         output_conf_vuln = ais.OutputConf(report_type=ais.get_type("report", "vulnerability"),
+#                                      file_format=ais.get_type("file_format", "json"),
+#                                      save_folder_path=report_path)
 
-        vul_report = aishield_client.save_job_report(job_id=job_id, output_config=output_conf_vuln)
+#         vul_report = aishield_client.save_job_report(job_id=job_id, output_config=output_conf_vuln)
         
-        output_conf_def = ais.OutputConf(report_type=ais.get_type("report", "defense"),
-                                     file_format=ais.get_type("file_format", "json"),
-                                     save_folder_path=report_path)
+#         output_conf_def = ais.OutputConf(report_type=ais.get_type("report", "defense"),
+#                                      file_format=ais.get_type("file_format", "json"),
+#                                      save_folder_path=report_path)
 
-        def_report = aishield_client.save_job_report(job_id=job_id, output_config=output_conf_def)
+#         def_report = aishield_client.save_job_report(job_id=job_id, output_config=output_conf_def)
         
-        output_conf_def_art = ais.OutputConf(report_type=ais.get_type("report", "defense_artifact"),
-                                     file_format=ais.get_type("file_format", "json"),
-                                     save_folder_path=report_path)
+#         output_conf_def_art = ais.OutputConf(report_type=ais.get_type("report", "defense_artifact"),
+#                                      file_format=ais.get_type("file_format", "json"),
+#                                      save_folder_path=report_path)
 
-        def_artifact_report = aishield_client.save_job_report(job_id=job_id, output_config=output_conf_def_art)
+#         def_artifact_report = aishield_client.save_job_report(job_id=job_id, output_config=output_conf_def_art)
 
-def get_x_api_key(org_id_val):
-    headers = {
-        'org_id': org_id_val
-    }
-    response = requests.get('https://api.aws.boschaishield.com/prod/api/ais/v1.5/get_aws_api_key', headers=headers)
-    if response.status_code == 200:
-        # Print the entire response content
-        x_api_key_data = response.json()        
-    else:
-        # Print the error status code and content
-        print(f"Error: {response.status_code}")
-        print(response.text)
-    return x_api_key_data['x_api_key']
+# def get_x_api_key(org_id_val):
+    
+#     headers = {
+#         'org_id': org_id_val
+#     }
+#     response = requests.get('https://api.aws.boschaishield.com/prod/api/ais/v1.5/get_aws_api_key', headers=headers)
+#     if response.status_code == 200:
+#         # Print the entire response content
+#         x_api_key_data = response.json()        
+#     else:
+#         # Print the error status code and content
+#         print(f"Error: {response.status_code}")
+#         print(response.text)
+#     return x_api_key_data['x_api_key']
 
 def get_job_meta_data(x_api_key_val, org_id_val, job_id_val):
+    '''
+        Returns the meta data of a job initiated.
+    '''
     headers = {
         'x-api-key': x_api_key_val,
         'org_id': org_id_val
@@ -196,9 +213,15 @@ def get_job_meta_data(x_api_key_val, org_id_val, job_id_val):
     job_meta_data.update({'JobID': job_id_val,'AttackType': job_meta_data_all['AttackType'], 'ModelInformation': job_meta_data_all['ModelInformation'], 'Time': job_meta_data_all['CreatedTimestamp'], 'AttackQueries': job_meta_data_all['NumerofAttackQueries'], 'VulnerabilityThreshold': job_meta_data_all['VulnerabiltiyThreshold']})    
     return job_meta_data  
 
-def upload_to_github(owner, repo, path, access_token, data):
-    
+def upload_to_github(path, data):
+    '''
+        Uploads the vulnerability report, defense report and jobmetadata report to ml-assessment-datastore.
+    '''
+    owner = 'asa1997'
+    repo = 'besecure-ml-assessment-datastore'
     url = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
+    access_token = '########' #### Paste github access token
+    
     json_string = json.dumps(data, indent=4)
     base64_content = base64.b64encode(json_string.encode()).decode()
     data = {
@@ -213,22 +236,143 @@ def upload_to_github(owner, repo, path, access_token, data):
     response = requests.put(url, json=data, headers=headers)
 
     if response.status_code == 201:
-        return "File with JSON contents successfully created/updated on GitHub!"
+        print("File {path} with JSON contents successfully created/updated on GitHub!")
     else:
-        return f"Failed to create/update file. Status code: {response.status_code}, Error: {response.text}"
+        print(f"Failed to create/update file under {path}. Status code: {response.status_code}, Error: {response.text}")
+
+def write_job_to_file(job_id_val):
+    '''
+        Saves the job_id of jobs in progress in a local file. Used to retrieve job_id of jobs in progress.
+    '''
+    job_folder = os.environ['HOME'] + '/.bes'
+    create_dir(job_folder)
+    job_file = job_folder +'/job_id.txt'
+    
+    if os.path.exists(job_file):
+        with open(job_file, 'r') as f:
+            file_contents = f.read()
+    
+        if job_id_val in file_contents:
+            print("job id exists")
+            return "job id exists"
+    
+    with open(job_file, 'a') as f:
+        f.write(job_id_val + '\n')  # Add a newline for clarity
+        print(f"{job_id_val} appended to the file:", job_file)
+
+def download_reports(job_id_val, org_id_val, x_api_key_val, report_type_val):
+    '''
+        Downloads assessment reports from aishield api.
+    '''
+    headers = {
+        'x-api-key': x_api_key_val,
+        'org_id': org_id
+    }
+    payload = {
+        'job_id': job_id_val,
+        'report_type': report_type_val,
+        'file_format': 3
+    }
+    
+    response = requests.get('https://api.aws.boschaishield.com/prod/api/ais/v1.5/job_status_detailed', headers=headers, params=payload)
+
+    if response.status_code == 200:
+        # Print the entire response content
+        report = response.json()
+        # print(job_meta_data_all)
+        return report
         
-# Define a route and its corresponding view function
+    else:
+        # Print the error status code and content
+        print(f"Error: {response.status_code}")
+        print(response.text)
+
+def delete_job_id(job_id_val):
+    '''
+        Deletes the job_id entry if the analysis is completed.
+    '''
+    print("delete job id")
+    file_path = os.environ['HOME'] + '/.bes/job_id.txt'
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    # Remove the specific entry from the list of lines
+    lines = [line for line in lines if line.strip() != job_id_val]
+
+    # Write the modified content back to the file
+    with open(file_path, 'w') as file:
+        file.writelines(lines)
+    
+
+def check_job_status():
+    '''
+        Checks whether an analysis is in progress or not. If completed downloads the report and pushes to github
+    '''
+    print("check job status")
+    vulnerability_report_path = 'models/bes-image-classification/fuzz-test/evasion/VulnerabilityReport1.json'
+    defense_report_path = 'models/bes-image-classification/fuzz-test/evasion/DefenceReport1.json'
+    
+    
+    job_file = os.environ['HOME'] + '/.bes/job_id.txt'
+    with open(job_file, 'r') as f:
+        file_contents = [line.strip() for line in f.readlines()]
+    
+    for job_id_val in file_contents:
+        print(job_id_val)
+        if job_id_val == '':
+            continue
+        headers = {
+            'x-api-key': x_api_key,
+            'org_id': org_id
+        }
+        payload = {
+            'job_id': job_id_val
+        }
+        response = requests.get('https://api.aws.boschaishield.com/prod/api/ais/v1.5/job_status_detailed', headers=headers, params=payload)
+
+        if response.status_code == 200:
+            # Print the entire response content
+            print("Response for job status: 200")
+            job_status = response.json()
+            # print(job_meta_data_all)
+            
+        else:
+            # Print the error status code and content
+            print(f"Error: {response.status_code}")
+            print(response.text)
+        
+        DefenseReport_Status = job_status['DefenseReport_Status']
+        VunerabilityEngine_Status = job_status['VunerabilityEngine_Status']
+        
+        if VunerabilityEngine_Status == 'completed' and DefenseReport_Status == 'completed':
+            delete_job_id(job_id_val)
+            defense_report = download_reports(job_id_val, org_id, x_api_key, 'defense')
+            vulnerability_report = download_reports(job_id_val, org_id, x_api_key, 'vulnerability')
+        
+            upload_to_github(vulnerability_report_path, vulnerability_report)
+            upload_to_github(defense_report_path, defense_report)
+            
+        else:
+            print("analysis in progress")
+
+schedule.every(10).seconds.do(check_job_status) # Scheduler to run check_job_status every 10 second.
+
+def run_scheduler():
+    '''
+        Method to run the scheduler
+    '''
+    print("running scheduler")
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+scheduler_thread = threading.Thread(target=run_scheduler)
+scheduler_thread.start()
+
+#Define a route and its corresponding view function
 @app.route('/ml/assessment/', methods=['POST'])
 def index():
-
-# def index(model_url, data_url, label_url, task, attack_type):
     data = request.get_json() 
-    # print(data)
-    org_id = '#########' #### Paste org id
-    owner = 'asa1997'
-    repo = 'besecure-ml-assessment-datastore'
-    job_meta_data_path = 'models/bes-image-classification/fuzz-test/JobMetadata.json'
-    access_token = '##########' #### Paste github access token
     ModelUrl = data['ModelUrl']
     DataUrl = data['DataUrl']
     LabelUrl = data['LabelUrl']
@@ -239,23 +383,15 @@ def index():
     zip_path = download_zip_files(ModelUrl, DataUrl, LabelUrl)
     upload_artifacts(zip_path, status, model_registration_repsone, aishield_client)
     job_id = model_analysis(task_type, analysis_type, aishield_client, model_registration_repsone)
-    x_api_key = get_x_api_key(org_id)
+    # x_api_key = get_x_api_key(org_id)
     job_meta_data = get_job_meta_data(x_api_key, org_id, job_id)
     job_meta_data.update({'ModelUrl': data['ModelUrl'], 'DataUrl': data['DataUrl'], 'LabelUrl': data['LabelUrl']})
-    output = upload_to_github(owner, repo, job_meta_data_path, access_token, job_meta_data)
-    return output
-    # report_path = os.environ['HOME']+"/reports"
-    # status ="success"
-    
-    # if os.path.isdir(report_path):
-    #     print("directory {} already exist".format(report_path))
-    # if os.path.isdir(report_path) is False:
-    #     os.mkdir(path=report_path)
-    #     print("directory {} created successfully".format(report_path))
-    # download_analysis_reports(status, report_path, aishield_client, job_id)
-    # return "Analysis in progress"
-    
+    upload_to_github(job_meta_data_path, job_meta_data)
+    # return job_meta_data
+    write_job_to_file(job_id)
+    return job_meta_data
 
 # Run the app if this file is executed directly
 if __name__ == '__main__':
     app.run(debug=True)
+    
